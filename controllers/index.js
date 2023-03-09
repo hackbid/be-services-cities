615a788ae46c4e7bb16dca7eb2a84598b6672916
mashayyik <mashayyik@gmail.com>
2023-03-09 13:00:28 +0700

    feat: getCityById

--- controllers/index.js
+++ controllers/index.js
@@ -1,32 +1,41 @@
-const api = require("../config/axios");
+const api = require('../config/axios');
 
 class Controller {
-  static async GetProvince(req, res) {
-    try {
-      const { data } = await api.get("/province");
-      res.status(200).json(data.rajaongkir.results);
-    } catch (err) {
-      res.status(500).json({ message: "Internal Server Error" });
+    static async GetProvince(req, res) {
+        try {
+            const { data } = await api.get('/province');
+            res.status(200).json(data.rajaongkir.results);
+        } catch (err) {
+            res.status(500).json({ message: 'Internal Server Error' });
+        }
     }
-  }
 
-  static async GetCity(req, res) {
-    try {
-      const { data } = await api.get("/city");
-      res.status(200).json(data.rajaongkir.results);
-    } catch (err) {
-      res.status(500).json({ message: "Internal Server Error" });
+    static async GetCity(req, res) {
+        try {
+            const { data } = await api.get('/city');
+            res.status(200).json(data.rajaongkir.results);
+        } catch (err) {
+            res.status(500).json({ message: 'Internal Server Error' });
+        }
     }
-  }
+    static async GetCityById(req, res) {
+        try {
+            const { id } = req.params;
+            const { data } = await api.get(`/city?cities_id=${req.params.id}`);
+            res.status(200).json(data.rajaongkir.results);
+        } catch (err) {
+            res.status(500).json({ message: 'Internal Server Error' });
+        }
+    }
 
-  static async PostCost(req, res) {
-    try {
-      const { data } = await api.post("/cost", req.body);
-      res.status(200).json(data.rajaongkir.results);
-    } catch (err) {
-      res.status(500).json({ message: "Internal Server Error" });
+    static async PostCost(req, res) {
+        try {
+            const { data } = await api.post('/cost', req.body);
+            res.status(200).json(data.rajaongkir.results);
+        } catch (err) {
+            res.status(500).json({ message: 'Internal Server Error' });
+        }
     }
-  }
 }
 
 module.exports = Controller;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-const router = require('express').Router()
-const Controller = require('../controllers/index')
+const router = require('express').Router();
+const Controller = require('../controllers/index');
 
-router.get('/province',Controller.GetProvince)
-router.get('/city',Controller.GetCity)
-router.post('/cost',Controller.PostCost)
+router.get('/province', Controller.GetProvince);
+router.get('/city', Controller.GetCity);
+router.get('/city/:id', Controller.GetCityById);
+router.post('/cost', Controller.PostCost);
 
-module.exports = router
+module.exports = router;
